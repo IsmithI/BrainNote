9834d2d65a3fd0660c5e88c303e8cdba35e03458
Oleg Bondar <olegbondar1998@gmail.com>
2017-03-31 14:49:39 +0300

    Deploy

--- src/main/java/ua/kiev/prog/WebApplication.java
+++ src/main/java/ua/kiev/prog/WebApplication.java
@@ -1,15 +1,16 @@
 package ua.kiev.prog;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by smith on 31.03.17.
  */
 @SpringBootApplication
+@ComponentScan("ua.kiev.prog")
 public class WebApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
